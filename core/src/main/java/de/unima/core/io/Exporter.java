82f2c5e1204b72e44894f7258ba476fcbe429742
Gregor Trefs <trefs@es.uni-mannheim.de>
2015-11-15 23:02:23 +0100

    Refactored IO
    Added missing type parameters for generic types deleted some blank lines

--- core/src/main/java/de/unima/core/io/Exporter.java
+++ core/src/main/java/de/unima/core/io/Exporter.java
@@ -1,6 +1,5 @@
 package de.unima.core.io;
 
 public interface Exporter<S extends DataSource> {
-
 	public void export();
 }
--- core/src/main/java/de/unima/core/io/Importer.java
+++ core/src/main/java/de/unima/core/io/Importer.java
@@ -3,7 +3,6 @@ package de.unima.core.io;
 import org.apache.jena.ontology.OntModel;
 
 public interface Importer<S extends DataSource> {
-	
 	public OntModel importData(S ds);
 	public String getID();
 }
