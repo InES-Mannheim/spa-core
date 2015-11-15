4c0668f52d512facb19ad5090129c25b00847a09
Gregor Trefs <trefs@es.uni-mannheim.de>
2015-11-15 23:09:20 +0100

    Added lightweight store abstraction
    This store abstraction takes functions which are performed against the
    wrapped store connection. When transactions are supported by the store,
    each operation is executed in a transactional context. If there is no
    support, just the operation is executed. The JenaTDBStore is an
    implementation of this pattern. It supports read and write transactions.
    This fact was also abstracted (cf. Lock and TransactionSupport).
    
    Note: The abstraction itself is not complete and leaking is part of the
    design.

---
+++ core/src/main/java/de/unima/core/storage/jena/JenaTDBStore.java
@@ -0,0 +1,56 @@
+package de.unima.core.storage.jena;
+
+import java.nio.file.Path;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.tdb.TDBFactory;
+
+import de.unima.core.storage.Lock;
+import de.unima.core.storage.Store;
+import de.unima.core.storage.StoreConnection;
+
+public class JenaTDBStore implements Store {
+
+	private Dataset dataset;
+
+	public JenaTDBStore(Path storageFolder) {
+		this.dataset = TDBFactory.createDataset(storageFolder.toString());
+	}
+	
+	@Override
+	public StoreConnection getConnection() {
+		return new JenaTdbStoreConnection();
+	}
+	
+	public final class JenaTdbStoreConnection implements StoreConnection {
+		
+		@Override
+		public boolean areTransactionsSupported() {
+			return true;
+		}
+		
+		@Override
+		public long beginTransaction(Lock lock) {
+			final ReadWrite readWrite = (lock == Lock.READ ? ReadWrite.READ: ReadWrite.WRITE);
+			JenaTDBStore.this.dataset.begin(readWrite);
+			return 0;
+		}
+		
+		@Override
+		public void commitTransaction(long transactionToken) {
+			JenaTDBStore.this.dataset.commit();
+		}
+		
+		@Override
+		public void rollbackTransaction(long transactionToken) {
+			JenaTDBStore.this.dataset.abort();
+		}
+		
+		@Override
+		public Object getWrappedStoreConnection() {
+			return JenaTDBStore.this.dataset;
+		}
+	}
+
+}
---
+++ core/src/test/java/de/unima/core/storage/StoreTest.java
@@ -0,0 +1,25 @@
+package de.unima.core.storage;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class StoreTest  {
+
+	@Test
+	public void noOpShouldBeCreated() {
+		final Store store = Store.noOp();
+		assertThat(store, is(notNullValue()));
+	}
+	
+	@Test
+	public void noOpShouldReturnEmpty() {
+		final Store store = Store.noOp();
+		final Optional<String> result = store.runWithConnection(connection -> "test");
+		assertThat(result.isPresent(), is(false));
+	}
+}
