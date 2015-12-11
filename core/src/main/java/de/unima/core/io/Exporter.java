5d8629f9b215a96d521a09feeae887ce5b40ae2f
Gregor Trefs <Gregor.Trefs@gmail.com>
2015-12-11 11:36:53 +0100

    Added JavaDoc to Importer and Exporter

--- core/src/main/java/de/unima/core/io/Exporter.java
+++ core/src/main/java/de/unima/core/io/Exporter.java
@@ -1,5 +1,10 @@
 package de.unima.core.io;
 
+/**
+ * Provides means to export data.
+ * 
+ * @param <T> format of the data.
+ */
 public interface Exporter<T> {
 	public T export();
 }
--- core/src/main/java/de/unima/core/io/Importer.java
+++ core/src/main/java/de/unima/core/io/Importer.java
@@ -2,6 +2,11 @@ package de.unima.core.io;
 
 import org.apache.jena.ontology.OntModel;
 
+/**
+ * {@code Importer} provides means to convert data into RDF.
+ * 
+ * @param <T> source of the importer
+ */
 public interface Importer<T> {
 	public OntModel importData(T dataSource);
 	public String getID();
