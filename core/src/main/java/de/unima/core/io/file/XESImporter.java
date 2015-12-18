f0cfdb92a6421405577268cbc187d0b8fb4f800c
Gregor Trefs <trefs@es.uni-mannheim.de>
2015-12-18 11:44:58 +0100

    Added InputStream support

--- core/src/main/java/de/unima/core/io/file/XESImporter.java
+++ core/src/main/java/de/unima/core/io/file/XESImporter.java
@@ -2,10 +2,9 @@ package de.unima.core.io.file;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.Model;
@@ -20,18 +19,23 @@ public class XESImporter implements FileBasedImporter<Model> {
 	private final XMLImporter dataImporter;
     
 	public XESImporter() {
-		xesOntology = new XSDImporter().importData(getFilePath("xml/xes.xsd").toFile());
+		try(InputStream stream = openStream("xml/xes.xsd")){
+			xesOntology = new XSDImporter().importData(stream);
+		} catch (IOException e) {
+			throw Throwables.propagate(e);
+		}
 		dataImporter = new XMLImporter(xesOntology);
 	}
 	
-	private static Path getFilePath(final String fileName) {
+	private static InputStream openStream(final String resourceName) {
+		final URL fileUrl = Resources.getResource(resourceName);
 		try {
-			return Paths.get(Resources.getResource(fileName).toURI());
-		} catch (URISyntaxException e) {
+			return Resources.asByteSource(fileUrl).openStream();
+		} catch (IOException e) {
 			throw Throwables.propagate(e);
 		}
 	}
-	
+
 	@Override
 	public Model importData(File dataSource) {
 		if(isEmpty(dataSource)){
--- core/src/main/java/de/unima/core/io/file/XSDImporter.java
+++ core/src/main/java/de/unima/core/io/file/XSDImporter.java
@@ -1,6 +1,7 @@
 package de.unima.core.io.file;
 
 import java.io.File;
+import java.io.InputStream;
 
 import org.apache.jena.ontology.OntModel;
 
@@ -11,10 +12,20 @@ public class XSDImporter implements FileBasedImporter<OntModel> {
 	@Override
 	public OntModel importData(File xmlSource) {
 		XSD2OWLMapper mapping = new XSD2OWLMapper(xmlSource);
-	    mapping.setObjectPropPrefix("");
+	    return convertXsdToOntology(mapping);
+	}
+	
+	public OntModel importData(InputStream xmlSource){
+		final XSD2OWLMapper mapping = new XSD2OWLMapper(xmlSource);
+		return convertXsdToOntology(mapping);
+	}
+
+	private OntModel convertXsdToOntology(XSD2OWLMapper mapping) {
+		mapping.setObjectPropPrefix("");
 	    mapping.setDataTypePropPrefix("");
 	    mapping.convertXSD2OWL();
 		return mapping.getOntology();
 	}
+	
 
 }
