9592fe95be918b92e35f8dc2d7dd73d89044a254
Gregor Trefs <trefs@es.uni-mannheim.de>
2015-12-18 17:06:17 +0100

    Default implentation for exportToFile

--- core/src/main/java/de/unima/core/io/file/FileBasedExporter.java
+++ core/src/main/java/de/unima/core/io/file/FileBasedExporter.java
@@ -1,11 +1,28 @@
 package de.unima.core.io.file;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import org.apache.jena.rdf.model.Model;
+
+import com.google.common.base.Throwables;
 
 import de.unima.core.io.Exporter;
 
 public interface FileBasedExporter<T extends Model> extends Exporter<T, File> {
+	
+	@Override
+	public default File export(T model) {
+		try {
+			final File file = Files.createTempFile("exported", null).toFile();
+			exportToFile(model, file);
+			return file;
+		} catch (IOException e) {
+			throw Throwables.propagate(e);
+		}
+	}
+
+	
 	File exportToFile(T data, File location);
 }
--- core/src/main/java/de/unima/core/io/file/RDFExporter.java
+++ core/src/main/java/de/unima/core/io/file/RDFExporter.java
@@ -3,24 +3,12 @@ package de.unima.core.io.file;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 
 import org.apache.jena.rdf.model.Model;
 
 import com.google.common.base.Throwables;
 
-public class RdfExporter implements FileBasedExporter<Model>{
-
-	@Override
-	public File export(Model model) {
-		try {
-			final File file = Files.createTempFile("exported-jena-model", null).toFile();
-			exportToFile(model, file);
-			return file;
-		} catch (IOException e) {
-			throw Throwables.propagate(e);
-		}
-	}
+public class RDFExporter implements FileBasedExporter<Model>{
 
 	@Override
 	public File exportToFile(Model data, File location) {
